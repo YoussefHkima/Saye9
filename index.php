0134aae3d241823ba2d1fdb6ae58d5a858cbb85f
YoussefHkima <youssefhkima29@gmail.com>
2023-10-10 16:43:27 +0100

    page produit

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+    $produit_nettoyage=array(
+    array('titel'=>'lave_main','description'=>'produit de nettoyage','image'=>'3.jpg'),
+    array('titel'=>'lave_vaiselle','description'=>'produit de nettoyage','image'=>'4.jpg'),
+    array('titel'=>'javel','description'=>'produit de nettoyage','image'=>'5.jpg')
+    array('titel'=>'machine à laver','description'=>'machine','image'=>'2.jpg')
+    array('titel'=>'ajax','description'=>'produit de nettoyage','image'=>'1.jpg')
+    array('titel'=>'air frêche','description'=>'produit de nettoyage','image'=>'6.jpg')
+    );
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class="conta">
+        <div>
+            
+        </div>
+        
+    </div>
+    
+</body>
+</html>
