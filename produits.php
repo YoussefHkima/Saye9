c18257c18ed17c5657b2ffb7c154646109f88af2
YoussefHkima <youssefhkima29@gmail.com>
2023-12-10 20:06:07 +0100

    add part of php on search navbar

--- produits.php
+++ produits.php
@@ -4,7 +4,7 @@
     $servername = "localhost";
     $username = "root";
     $password = "";
-    $dbname = "sayek";    
+    $dbname = "sayek";     
     
     // Create connection
     $conn = new mysqli($servername, $username, $password, $dbname);
